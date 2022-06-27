15affac631b410b8e633fb2862191c225869defc
Carlos Doblas <carlosdoblas99@correo.ugr.es>
2022-06-27 15:55:54 +0200

    Se realizan pequeñas correciones y se introduce archivo de configuración rviz

--- Proyecto/proyecto_tfg/src/tuw_aruco/src/reduce_cameras.cpp
+++ Proyecto/proyecto_tfg/src/tuw_aruco/src/reduce_cameras.cpp
@@ -12,6 +12,17 @@ ros::Publisher marker_pub;
 void action(const geometry_msgs::PoseStamped::ConstPtr& msg_cam1, const geometry_msgs::PoseStamped::ConstPtr& msg_cam2)
 {
     geometry_msgs::Pose result;
+    geometry_msgs::PoseStamped pose_cam_1, pose_cam_2;
+    pose_cam_1.pose.position.x = msg_cam1->pose.position.x;
+    pose_cam_1.pose.position.y = -msg_cam1->pose.position.z;
+    pose_cam_1.pose.position.z = -msg_cam1->pose.position.y;
+
+    pose_cam_2.pose.position.x = msg_cam2->pose.position.x;
+    pose_cam_2.pose.position.y = -msg_cam2->pose.position.z;
+    pose_cam_2.pose.position.z = -msg_cam2->pose.position.y;
+
+    double factor = 3;
+        
     bool found = true;
     if (msg_cam1->pose.position.x == -99.99 && msg_cam2->pose.position.x == -99.99 )
     {
@@ -22,29 +33,32 @@ void action(const geometry_msgs::PoseStamped::ConstPtr& msg_cam1, const geometry
     }
     else if (msg_cam1->pose.position.x != -99.99 && msg_cam2->pose.position.x == -99.99)
     {
-        result.position.x = msg_cam1->pose.position.x;
-        result.position.y = msg_cam1->pose.position.y;
-        result.position.z = msg_cam1->pose.position.z;
+
+        result.position = pose_cam_1.pose.position;
+        // result.position.x = msg_cam1->pose.position.x;
+        // result.position.y = msg_cam1->pose.position.y;
+        // result.position.z = msg_cam1->pose.position.z;
     }
     else if (msg_cam2->pose.position.x != -99.99 && msg_cam1->pose.position.x == -99.99)
     {
-        result.position.x = msg_cam2->pose.position.x;
-        result.position.y = msg_cam2->pose.position.y;
-        result.position.z = msg_cam2->pose.position.z;
+        result.position = pose_cam_2.pose.position;
+
+        // result.position.x = msg_cam2->pose.position.x;
+        // result.position.y = msg_cam2->pose.position.y;
+        // result.position.z = msg_cam2->pose.position.z;
     }
     else
     {
-        result.position.x = (msg_cam1->pose.position.x + msg_cam2->pose.position.x) / 2;
-        result.position.y = (msg_cam1->pose.position.y + msg_cam2->pose.position.y) / 2;
-        result.position.z = (msg_cam1->pose.position.z + msg_cam2->pose.position.z) / 2;
-
+        result.position.x = ((pose_cam_1.pose.position.x + pose_cam_2.pose.position.x) / 2) * factor;
+        result.position.y = ((pose_cam_1.pose.position.y + pose_cam_2.pose.position.y) / 2) * factor;
+        result.position.z = ((pose_cam_1.pose.position.z + pose_cam_2.pose.position.z) / 2) * factor;
     }
 
     if (found){
         visualization_msgs::Marker marker;
         uint32_t shape = visualization_msgs::Marker::SPHERE;
 
-        marker.header.frame_id = "/my_frame";
+        marker.header.frame_id = "/world";
         marker.header.stamp = ros::Time::now();
         marker.ns = "basic_shapes";
         marker.id = 0;
@@ -55,9 +69,9 @@ void action(const geometry_msgs::PoseStamped::ConstPtr& msg_cam1, const geometry
 
         marker.pose = result;
 
-        marker.scale.x = 0.1;
-        marker.scale.y = 0.1;
-        marker.scale.z = 0.1;
+        marker.scale.x = 0.3;
+        marker.scale.y = 0.3;
+        marker.scale.z = 0.3;
 
         marker.color.r = 0.0f;
         marker.color.g = 0.0f;
@@ -81,7 +95,7 @@ int main(int argc, char** argv)
     marker_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker", 1);
 
     message_filters::Subscriber<geometry_msgs::PoseStamped> cam1_sub(nh, "/cam_1/result", 1);
-    message_filters::Subscriber<geometry_msgs::PoseStamped> cam2_sub(nh, "/cam_2/result", 1);
+    message_filters::Subscriber<geometry_msgs::PoseStamped> cam2_sub(nh, "/cam_1/result", 1);
     TimeSynchronizer<geometry_msgs::PoseStamped, geometry_msgs::PoseStamped> sync(cam1_sub, cam2_sub, 10);
     sync.registerCallback(boost::bind(&action, _1, _2));
 
